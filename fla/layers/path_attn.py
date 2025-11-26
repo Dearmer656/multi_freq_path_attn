0d4b59e78da6fd63862c04914c06603bcaf7c39a
Dearmer656 <sunhongyu008@hotmail.com>
2025-11-27 08:54:35 +0900

    comments

--- fla/layers/path_attn.py
+++ fla/layers/path_attn.py
@@ -490,11 +490,11 @@ def spectral_distill_over_L(
     with torch.no_grad():
         A_t, A_t_log = spectrum_over_T_multi(teacher)   # [B,Q,K,H,D]  teacher不反传
     A_s, A_s_log = spectrum_over_T_multi(student)       # [B,Q,K,H,D]
-    A_t_scale = aggregate_spectrum_by_scale(A_t, group_size=8, distill_teacher=distill_teacher)  # [H, S, K]
-    A_s_scale = aggregate_spectrum_by_scale(A_s, group_size=8, distill_teacher=distill_teacher)
+    # A_t_scale = aggregate_spectrum_by_scale(A_t, group_size=8, distill_teacher=distill_teacher)  # [H, S, K]
+    # A_s_scale = aggregate_spectrum_by_scale(A_s, group_size=8, distill_teacher=distill_teacher)
     os.makedirs('freq_analysis_logs/spectrum_domain_plots', exist_ok=True)
-    # plot_out_head_dim_groups_or_grouped(A_t[:, 0, ...], save_dir='freq_analysis_logs/spectrum_domain_plots', name=f"layer{layer_idx}_teacher", distill_teacher=distill_teacher)
-    # plot_out_head_dim_groups_or_grouped(A_s[:, 0, ...], save_dir='freq_analysis_logs/spectrum_domain_plots', name=f"layer{layer_idx}_student", distill_teacher=distill_teacher)
+    plot_out_head_dim_groups_or_grouped(A_t[:, 0, ...], save_dir='freq_analysis_logs/spectrum_domain_plots', name=f"layer{layer_idx}_teacher", distill_teacher=distill_teacher)
+    plot_out_head_dim_groups_or_grouped(A_s[:, 0, ...], save_dir='freq_analysis_logs/spectrum_domain_plots', name=f"layer{layer_idx}_student", distill_teacher=distill_teacher)
     # t_mean, s_mean, kl_mat, row_ind, col_ind = match_heads_by_kl_over_S(
     #     A_t_scale, A_s_scale
     # )
@@ -508,9 +508,9 @@ def spectral_distill_over_L(
     #     title_prefix=f"Layer {layer_idx}"
     # )
     # pdb.set_trace()
-    if layer_idx == 0:
-        print("layer0 teacher power sum:", A_t_scale.abs().sum())
-        print("layer0 student power sum:", A_s_scale.abs().sum())
+    # if layer_idx == 0:
+        # print("layer0 teacher power sum:", A_t_scale.abs().sum())
+        # print("layer0 student power sum:", A_s_scale.abs().sum())
     # pdb.set_trace()
     stats = spectrum_stats_teacher_student(A_t_scale, A_s_scale, low_ratio=0.25)
     all_stats_per_layer.append(stats)
@@ -1033,12 +1033,14 @@ class PaTHAttention(nn.Module):
                 g = rearrange(g, 'b t hq -> b t hq 1').repeat(1, 1, self.r, 1).view(g.shape[0], g.shape[1], -1)
 
             # 核心 op
-
+            gamma_stats = compute_gamma_stats(w=w, beta=beta)
+            plot_gamma_lambda_hist(gamma_stats['gamma'], gamma_stats['lambda_dir'], ratio_contractive=gamma_stats['ratio_contractive'], title_prefix=f"wavelet_distill_layer{self.layer_idx}", outdir='gamma_lambda_hist')
             o, _ = parallel_path_attn(q=q, k=k, v=v, w=w, beta=beta, g=g, cu_seqlens=cu_seqlens)
             # if (self.layer_idx < 2) and self.training:
             offsets = (1, 8, 16, 32)
             # idx = [k.size(1) - o for o in offsets]       # 绝对下标
             # Q_sel = q[:, idx, :, :]
+            print(f'layer{self.layer_idx} analysis!!!!')
             with torch.no_grad():
                 if self.config.distill_teacher == 'rotary':
                     dim_wise_scores = self.rotary_emb.rotate_queries_or_keys(q.permute(0, 2, 1, 3).contiguous()) * self.rotary_emb.rotate_queries_or_keys(k.permute(0, 2, 1, 3).contiguous())
@@ -1052,9 +1054,9 @@ class PaTHAttention(nn.Module):
             #     # wavelet_scores = compute_wavelet_scores_multi_causal(Q_sel, k, wavelet_decay_table[:, -1, :], query_indices=idx, table_direction="near_to_far")
             #     wavelet_scores = compute_wavelet_scores_batched(q[:, -1, ...], k, wavelet_decay_table[:, -1, :])
             
-            path_attn_scores = compute_path_scores_batched_last_q(q[:, -1, ...], k, w, beta)
-            diff = path_attn_scores[..., 1:] - path_attn_scores[..., :-1]
-            E_diff = torch.mean(diff**2)
+            # path_attn_scores = compute_path_scores_batched_last_q(q[:, -1, ...], k, w, beta)
+            # diff = path_attn_scores[..., 1:] - path_attn_scores[..., :-1]
+            # E_diff = torch.mean(diff**2)
             # print(E_diff)
             # pdb.set_trace()
             # teacher_scores = F.softmax(teacher_scores, dim=-3)
@@ -1062,9 +1064,11 @@ class PaTHAttention(nn.Module):
             # plot_out_head_dim_groups_or_grouped(path_attn_scores, save_dir='temporal_domain_plots', name=f"layer{self.layer_idx}_student", distill_teacher=self.config.distill_teacher)
             # plot_out_head_dim_groups_or_grouped(teacher_scores, save_dir='temporal_domain_plots', name=f"layer{self.layer_idx}_teacher", distill_teacher=self.config.distill_teacher)
             
-            dis_loss = spectral_distill_over_L(path_attn_scores.unsqueeze(1), teacher_scores.unsqueeze(1), self.layer_idx, distill_teacher=self.config.distill_teacher)
+            # dis_loss = spectral_distill_over_L(path_attn_scores.unsqueeze(1), teacher_scores.unsqueeze(1), self.layer_idx, distill_teacher=self.config.distill_teacher)
             # else:
-            #     dis_loss = torch.tensor(0.0, device=q.device, dtype=q.dtype)
+            dis_loss = torch.tensor(0.0, device=q.device, dtype=q.dtype)
+            if self.layer_idx == 11:
+                os._exit(0)
             o = rearrange(o, 'b t (h r) d -> b t (h r d)', r=self.r)
             o = self.o_proj(o)
             return o, None, past_key_values, dis_loss
