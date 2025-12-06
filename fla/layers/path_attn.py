ce9e9694322803fd58fd1d4cf2bbee8fdf58f165
Dearmer656 <sunhongyu008@hotmail.com>
2025-12-06 15:04:48 +0900

    distill_in_which_layer

--- fla/layers/path_attn.py
+++ fla/layers/path_attn.py
@@ -1127,7 +1127,7 @@ class PaTHAttention(nn.Module):
             # 核心 op
 
             o, _ = parallel_path_attn(q=q, k=k, v=v, w=w, beta=beta, g=g, cu_seqlens=cu_seqlens)
-            if (self.layer_idx < self.config.distill_in_which_layers) and self.training:
+            if (self.layer_idx == self.config.distill_in_which_layers) and self.training:
                 if self.config.temp_loss_coe != 0:
                     i_idx, j_idx, delta = sample_index_pairs(self.config.block_size, self.config.sample_num)
                     batch_idx = torch.randint(
